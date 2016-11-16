14d0810f791caff910dfc3566ac91563d78623a0
wangshsion <992322084@qq.com>
2016-11-16 20:08:41 +0800

    edit

---
+++ dist/js/run2.0.js
@@ -0,0 +1,53 @@
+function getStyle(obj,attr){
+	if(obj.currentStyle){
+		return obj.currentStyle[attr]
+	}
+
+	else{
+		return getComputedStyle(obj,true)[attr]
+	}
+
+}
+
+
+
+function run(obj,json,fnEnd){
+	var timer;
+	clearInterval(obj.timer);
+	obj.timer=setInterval(function(){
+		var stop=true;
+	for(var attr in json){
+		cur=0;
+		if(attr=='opacity'){
+			cur=(parseFloat(getStyle(obj,attr)*100))
+		}
+
+		else{
+
+			cur=parseInt(getStyle(obj,attr));
+		}
+		
+		var speed=(json[attr]-cur)/3;
+		speed=speed>0?Math.ceil(speed):Math.floor(speed);
+		if(json[attr]!=cur)stop=false;
+		
+		
+			if(attr=='opacity'){
+			obj.style.filter='alpha('+(cur+speed)+')'
+			obj.style[attr]=(cur+speed)/100;
+			}
+
+			else{
+
+				obj.style[attr]=cur+speed+'px';
+			}
+		
+		}
+		if(stop){
+			clearInterval(obj.timer);
+			if(fnEnd)fnEnd();
+		}
+
+	},30);
+	
+}
